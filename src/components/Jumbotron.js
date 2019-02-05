a7b7d7e93b865fde47158da1f27db598f53376f8
tayloa <halfnote1004@gmail.com>
2019-02-05 16:52:53 -0500

    Added starting code and some styling for a Jumbotron component.

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './Jumbotron.css';
+
+/*
+ * Jumbotron to display a welcome message to the user in the PageFrame.
+ */
+class Jumbotron extends Component  {
+
+  render() {
+    return (
+      <div className="jumboton">
+        <h1> Select from one of our many curriculm to get started!  </h1>
+      </div>
+    );
+  }
+}
+
+export default Jumbotron;
