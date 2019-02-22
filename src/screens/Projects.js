703889705290a4e49020c54c62c979bc2d64533d
mikbukow <bukowm2@rpi.edu>
2019-02-22 16:24:20 -0500

    Adding Project and Projects page

--- src/screens/Projects.js
+++ src/screens/Projects.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import {Button, Col} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import '../components/Projects.css';
+
 
 import PageFrame from '../components/PageFrame';
 
@@ -8,6 +12,20 @@ class Projects extends Component {
         return (
             <PageFrame>
                 <h2>Projects</h2>
+                <Col>
+					<h3> Python </h3>
+						<Link to= "/projects/:id">
+							<Button bsStyle= "primary">Project Name </Button>
+						</Link>
+					<h3> C++ </h3>
+						<Link to= "/projects/:id">
+							<Button bsStyle= "primary">Project Name </Button>
+						</Link>
+					<h3> Java </h3>
+						<Link to= "/projects/:id">
+							<Button bsStyle= "primary">Project Name </Button>
+						</Link>
+				</Col>
             </PageFrame>
         );    
     }
