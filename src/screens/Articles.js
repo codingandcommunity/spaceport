fe59603074d7aa0562d9432284ad0ff11d7d6391
tayloa <halfnote1004@gmail.com>
2019-03-29 17:10:56 -0400

    Added code to fetch a single article and all articles. Need to use package to render markdown.

--- src/screens/Articles.js
+++ src/screens/Articles.js
@@ -6,26 +6,67 @@ class Articles extends Component {
     constructor(props) {
       super(props);
       this.state = {
-        articles: []
+        articles: [],
+        article: null,
+        content: null
       }
     }
 
     componentDidMount() {
       // TODO: MAKE request for test-articles repo with github api
       // https://api.github.com/repositories/177862169/contents/test.md
-      // TODO: display each article's title
-      axios.get('/__mocks/curriculums.json')
+      axios.get('https://api.github.com/repositories/177862169/contents/test.md')
         .then(res => {
-            const articles = res.data;
-            this.setState({ articles });
+            var article = res.data;
+            var content = atob(article.content)
+            this.setState({ article });
+            this.setState({ content });
+            console.log(content);
         });
+
+      const ignore = ["LICENSE", "README.md"];
+      // axios.get('https://api.github.com/repositories/177862169/contents/')
+      //   .then(res => {
+      //       var articles = res.data;
+      //       this.setState({ articles });
+      //   });
+
+      // TODO: display each article's title
+      // axios.get('/__mocks/curriculums.json')
+      //   .then(res => {
+      //       const articles = res.data;
+      //       this.setState({ articles });
+      //   });
     }
 
     render() {
       return (
         <PageFrame>
-          <h3>  Articles List </h3>
-          <p> Articles Here! </p>
+          <h1> Articles List </h1>
+            {
+             this.state.articles.map( (article) => {
+                 return (
+                 <li key={article.name}>
+                   <a href={article.url}>
+                     {article.name}
+                   </a>
+                 </li>
+               )
+             })
+            }
+          <ul>
+            {
+             this.state.articles.map( (article) => {
+                 return (
+                 <li key={article.name}>
+                   <a href={article.url}>
+                     {article.name}
+                   </a>
+                 </li>
+               )
+             })
+            }
+          </ul>
         </PageFrame>
       );
     }
